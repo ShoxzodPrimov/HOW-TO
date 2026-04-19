b2df05058357a30aa1526ec0610a802d46b52819
HTML-CSS-Dev <ranaldu456@gmail.com>
2026-04-19 12:49:06 +0500

    fix: fix the number of list

--- Curtain Menu/index.js
+++ Curtain Menu/index.js
@@ -1,11 +1,16 @@
 const menuBtn = document.getElementById("menuBtn");
 const closeBtn = document.getElementById("close-btn");
 const navPanel = document.querySelector("nav");
+const list = document.querySelector('ul');
+const listItems = ["About" , "Services" , "Clients" , "Contact"];
 
-menuBtn.onclick = () => {
+
+listItems.forEach( items => list.innerHTML +=`<li class="cursor-pointer text-3xl hover:text-white duration-100 ease-in">${items}</li> `);
+
+
+menuBtn.onclick = () => 
     navPanel.classList.replace("w-0" , "w-[100vw]");
-}
 
-closeBtn.onclick = () => {
+
+closeBtn.onclick = () =>
     navPanel.classList.replace("w-[100vw]" , "w-0");
-}
