8efc584e198a7db4c3f8172ad6378b4c54d7777c
HTML-CSS-Dev <ranaldu456@gmail.com>
2026-04-20 09:00:36 +0500

    first commit

--- Menus/Curtain Menu/index.js
+++ Menus/Curtain Menu/index.js
@@ -7,10 +7,6 @@ const listItems = ["About" , "Services" , "Clients" , "Contact"];
 
 listItems.forEach( items => list.innerHTML +=`<li class="cursor-pointer text-2xl hover:text-white duration-100 ease-in">${items}</li> `);
 
+menuBtn.onclick = () => navPanel.id === "width" ? navPanel.classList.replace("w-0" , "w-[200px]") : navPanel.classList.replace("h-0" , "h-[100vh]")
 
-menuBtn.onclick = () => 
-    navPanel.id === "width" ? navPanel.classList.replace("w-0" , "w-[200px]") : navPanel.classList.replace("h-0" , "h-[100vh]")
-
-
-closeBtn.onclick = () =>
-    navPanel.id === "width" ? navPanel.classList.replace("w-[200px]" , "w-0") : navPanel.classList.replace("h-[100vh]" , "h-0") 
+closeBtn.onclick = () => navPanel.id === "width" ? navPanel.classList.replace("w-[200px]" , "w-0") : navPanel.classList.replace("h-[100vh]" , "h-0") 
