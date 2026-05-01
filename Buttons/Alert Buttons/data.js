4cd09224cd97cfb1a8a65b48a4d38823217c9f55
HTML-CSS-Dev <ranaldu456@gmail.com>
2026-05-01 17:28:08 +0500

    add: Buttons/ Alert Buttons

---
+++ Buttons/Alert Buttons/data.js
@@ -0,0 +1,30 @@
+export const btnList = [ 
+        {
+            text:"Succes",
+            color:"#04AA6D",
+            hoverColor:"#46a049",
+        },
+
+        {
+            text:"Info",
+            color:"#2196F3",
+            hoverColor:"#0b7dda",
+        },
+        {
+            text:"Warning",
+            color:"#ff9800",
+            hoverColor:"#e68a00",
+        } , 
+        
+        {
+            text:"Danger",
+            color:"#f44336",
+            hoverColor:"#da190b",
+        },
+
+        {
+            text:"Default",
+            color:"#e7e7e7",
+            hoverColor:"#ddd",
+        }
+    ];
