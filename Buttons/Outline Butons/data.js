bb8194ca3503ca6b83060aa9769b5b08e7fbd201
HTML-CSS-Dev <ranaldu456@gmail.com>
2026-05-01 17:54:56 +0500

    add: Buttons/ Outline BUttons

---
+++ Buttons/Outline Butons/data.js
@@ -0,0 +1,28 @@
+export const btnList = [ 
+        {
+            text:"Succes",
+            color:"#04AA6D",
+        },
+
+        {
+            text:"Info",
+            color:"#2196F3",
+        },
+        {
+            text:"Warning",
+            color:"#ff9800",
+            hoverColor:"#e68a00",
+        } , 
+        
+        {
+            text:"Danger",
+            color:"#f44336",
+            hoverColor:"#da190b",
+        },
+
+        {
+            text:"Default",
+            color:"#e7e7e7",
+            hoverColor:"#ddd",
+        }
+    ];
